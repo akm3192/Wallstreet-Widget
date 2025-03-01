a07d998bf3811f1e2a10ab5359ae769e4fcc2bf1
ds4836 <ds4836@g.rit.edu>
2025-03-01 18:32:50 -0500

    issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { IonApp, IonPage, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+import { IonApp, IonPage, IonRouterOutlet, IonSplitPane, IonContent, IonItem, IonLabel, IonInput, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route, useLocation } from 'react-router-dom';
 import Menu from './components/Menu';
 import Page from './pages/Page';
-import SignIn from './pages/SignIn';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,21 +33,31 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { useState } from 'react';
 
 setupIonicReact();
 
+const SignIn: React.FC = () => {
+const [username, setUsername] = useState<string>('');
+const [password, setPassword] = useState<string>('');
+
+  return (
+    <IonContent>
+      <IonItem>
+        <IonLabel position="floating">Username</IonLabel>
+        <IonInput value={username} onIonChange={(e)=> setUsername(e.detail.value!)}></IonInput>
+      </IonItem>
+      <IonItem>
+        <IonLabel position="floating">Password</IonLabel>
+        <IonInput value={password} onIonChange={(e)=> setPassword(e.detail.value!)}></IonInput>
+      </IonItem>
+    </IonContent>
+  );
+}
 
 const App: React.FC = () => {
   return (
     <IonApp>
-
-      <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/signin" exact={true}>
-              <SignIn></SignIn>
-        </Route>
-        </IonRouterOutlet>
-      </IonReactRouter>
 
       <IonReactRouter>
         <IonSplitPane contentId="main">
@@ -61,6 +70,9 @@ const App: React.FC = () => {
             <Route path="/folder/:name" exact={true}>
               <Page />
             </Route>
+            <Route path="/signin" exact={true}>
+              <SignIn></SignIn>
+        </Route>
 
           </IonRouterOutlet>
         </IonSplitPane>
